67485dbce7c16ec9a6e3467a4cfb7a5007a7264a
MAGIkCODES <justiceodoyi@gmail.com>
2023-12-21 07:41:01 +0100

    main coomit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+
+<!-- COMPOSITION API -->
+<script setup>
+import {ref} from "vue"
+
+const count = ref(0);
+
+const addBtn = () => {
+  count.value = count.value +1
+}
+
+const subtractBtn = () => {
+  count.value = count.value -1
+}
+</script>
+
+<template>
+  <div class="header">
+   
+    <main>
+      <h1>this is the current count...</h1>
+      <h2>{{ count }}</h2>
+      <button @click="subtractBtn">-</button>
+      <button @click="addBtn">+</button>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  text-align: center;
+}
+</style>
